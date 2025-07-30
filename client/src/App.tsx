5989b9029c10cac7917e1b2bdc5351c41071840e
rmkane <rmkane@proton.me>
2025-07-30 19:39:31 -0400

    Add tailwind and prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-4xl font-bold">Vite + React</h1>
+      <div className="card flex flex-col items-center gap-2">
+        <button
+          className="rounded-md bg-blue-500 px-4 py-2 text-white"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
